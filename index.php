6374841041913ea42707b00e0760e87529b95e03
Ivan Grigorov <10940601+IvanGrigorov@users.noreply.github.com>
2018-03-19 20:30:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include_once(".\DI\Lib\DIContract.php");
+        include_once(dirname(__FILE__)."/DI/Lib/IContract.php");
         $contract =  DIContract::getInstance();
         $test = $contract->getInjectionWithParams("IURLParser", 
         ["params" => array([
